659496bff6991f94d85b193981f40e8e028c1db1
dantweb <daniil.tkachev@gmail.com>
2026-08-26 10:20:08 +0200

    Initial Commit

---
+++ SayAgainTests/SayAgainTests.swift
@@ -0,0 +1,19 @@
+//
+//  SayAgainTests.swift
+//  SayAgainTests
+//
+//  Created by Daniil Tkachev on 26.08.26.
+//
+
+import Testing
+@testable import SayAgain
+
+struct SayAgainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
